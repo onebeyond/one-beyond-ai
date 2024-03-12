a9d99e7658ff4e6ee1b15eb02b986f28c10dd2a7
loveirobert <loveirobert@gmail.com>
2024-03-12 21:39:34 +0100

    use mime types from text extractor

--- modules/text-extractor/src/lib/text-extractor.spec.ts
+++ modules/text-extractor/src/lib/text-extractor.spec.ts
@@ -1,9 +1,11 @@
-import {TextExtractor} from './text-extractor';
+import { TextExtractor } from './text-extractor';
+import { MimeType } from '@one-beyond-ai/mime-type';
 
-const textExtractor = new TextExtractor();
+const textExtractor = new TextExtractor(new MimeType());
 
 describe('TextExtractor', () => {
-  it('should work', () => {
-    
+  it('should extract text properly from pdf document', () => {
+    const extracted = textExtractor.extractText('../test-files/test.pdf');
+    expect(extracted).toBe('PDF text');
   });
 });
--- modules/text-extractor/src/lib/text-extractor.ts
+++ modules/text-extractor/src/lib/text-extractor.ts
@@ -1,5 +1,18 @@
-export class TextExtractor {
-  constructor() {
+import { MimeType } from "@one-beyond-ai/mime-type";
 
+export class TextExtractor {
+  constructor(private mimeType: MimeType) {}
+  public extractText(filePath: string): string {
+    const mimeType = this.mimeType.getFileMIMEType(filePath);
+    if (mimeType === "application/pdf") {
+      return "PDF text";
+    }
+    if (mimeType === "application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
+      return "DOCX text";
+    }
+    if (mimeType === "application/x-iwork-pages-sffpages") {
+      return "PAGES text";
+    }
+    return "";
   }
 }
